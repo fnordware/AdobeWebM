8e360506513eadd973e23543996fbeab94fc882c
Brendan Bolles <brendan@fnordware.com>
2015-02-05 15:48:47 -0800

    Mac highbitdepth configs

--- xcode/xcode3/ext/libvpx_config/vpx_config.c
+++ xcode/xcode3/ext/libvpx_config/vpx_config.c
@@ -5,5 +5,5 @@
 /* tree. An additional intellectual property rights grant can be found */
 /* in the file PATENTS.  All contributing project authors may */
 /* be found in the AUTHORS file in the root of the source tree. */
-static const char* const cfg = "";
+static const char* const cfg = "--enable-vp9-highbitdepth";
 const char *vpx_codec_build_config(void) {return cfg;}
