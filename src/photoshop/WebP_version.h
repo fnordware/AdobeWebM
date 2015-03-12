c02a9e423e3302074c09f76671705a00885caa48
Brendan Bolles <brendan@fnordware.com>
2015-03-12 10:53:40 -0700

    Update Mac version info

--- src/photoshop/WebP_version.h
+++ src/photoshop/WebP_version.h
@@ -41,12 +41,12 @@
 
 #define WebP_Major_Version 0
 #define WebP_Minor_Version 5
-#define WebP_Version_String "0.5b5"
+#define WebP_Version_String "0.5b7"
 #define WebP_Build_Date __DATE__
-#define WebP_Build_Date_Manual "16 December 2013"
-#define WebP_Build_Complete_Manual "v0.5b5 - " WebP_Build_Date
-#define WebP_Copyright_Year "2013"
-#define WebP_Build_Year "2013"
+#define WebP_Build_Date_Manual "12 March 2015"
+#define WebP_Build_Complete_Manual "v0.5b7 - " WebP_Build_Date
+#define WebP_Copyright_Year "2013-2015"
+#define WebP_Build_Year "2015"
 
 #define WebP_Description "A file format module for writing WebP files in Adobe Photoshop."
 
